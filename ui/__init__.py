ddf4545ecf891212b24bf95f76220cd383eacb22
PillB <pablo.illescas.buendia@gmail.com>
2025-11-24 12:27:37 -0500

    Add accordion CollapsibleSection component

---
+++ ui/__init__.py
@@ -0,0 +1,5 @@
+"""Paquete principal de componentes de interfaz."""
+
+from .layout import CollapsibleSection, register_styles as register_layout_styles
+
+__all__ = ["CollapsibleSection", "register_layout_styles"]
