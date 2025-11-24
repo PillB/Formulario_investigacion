6d868c04c80191b6266b15ea4c787d583a36eb3f
PillB <pablo.illescas.buendia@gmail.com>
2025-11-24 00:54:55 -0500

    Enhance theme styling for notebooks and scrollbars

--- ui/config.py
+++ ui/config.py
@@ -4,9 +4,9 @@ from __future__ import annotations
 
 import tkinter as tk
 
-FONT_BASE = ("TkDefaultFont", 11)
-FONT_HEADER = ("TkHeadingFont", 12, "bold")
-ROW_PADY = 6
-COL_PADX = 8
+FONT_BASE = ("TkDefaultFont", 12)
+FONT_HEADER = ("TkHeadingFont", 13, "bold")
+ROW_PADY = 8
+COL_PADX = 10
 
 __all__ = ["FONT_BASE", "FONT_HEADER", "ROW_PADY", "COL_PADX"]
