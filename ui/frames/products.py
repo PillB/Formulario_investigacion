8e3d5313c119e0b30dcd5d367d010aad6fb729ed
PillB <pablo.illescas.buendia@gmail.com>
2025-12-11 19:47:38 -0500

    Start clients and products collapsed by default

--- ui/frames/products.py
+++ ui/frames/products.py
@@ -148,6 +148,7 @@ class InvolvementRow:
         return create_collapsible_card(
             parent,
             title="",
+            open=False,
             on_toggle=lambda _section: self._sync_section_title(),
             log_error=lambda exc: log_event(
                 "validacion",
@@ -305,6 +306,7 @@ class InvolvementRow:
         return create_collapsible_card(
             parent,
             title="",
+            open=False,
             on_toggle=lambda _section: self._sync_section_title(),
             log_error=lambda exc: log_event(
                 "validacion",
@@ -831,6 +833,7 @@ class ClaimRow:
         return create_collapsible_card(
             parent,
             title="",
+            open=False,
             on_toggle=lambda _section: self._sync_section_title(),
             log_error=lambda exc: log_event(
                 "validacion",
@@ -1737,6 +1740,7 @@ class ProductFrame:
         return create_collapsible_card(
             parent,
             title="",
+            open=False,
             on_toggle=lambda _section: self._sync_section_title(),
             log_error=lambda exc: log_event(
                 "validacion",
